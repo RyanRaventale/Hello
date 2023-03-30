f1c8f987987532ee6cd72ef536431039f67e9891
unknown <gsm101frt@gmail.com>
2023-03-30 20:40:32 +0300

    add file

---
+++ head.java
@@ -0,0 +1,9 @@
+public class head {
+    public static void main(String[] args) {
+        System.out.println("Hi world!");
+        String firstName = "Victor ";
+        String lastName = "Voytovik";
+        String fullName = firstName + lastName;
+        System.out.println("My name is " + fullName);
+    }
+}
