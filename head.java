c5730e2a8e60d84de8ce4110a1880cbde38070ad
unknown <gsm101frt@gmail.com>
2023-03-30 21:47:33 +0300

    update file

--- head.java
+++ head.java
@@ -1,8 +1,8 @@
 public class head {
     public static void main(String[] args) {
         System.out.println("Hi world!");
-        String firstName = "Victor ";
-        String lastName = "Voytovik";
+        String firstName = "Flamel ";
+        String lastName = "Rollinstein";
         String fullName = firstName + lastName;
         System.out.println("My name is " + fullName);
     }
